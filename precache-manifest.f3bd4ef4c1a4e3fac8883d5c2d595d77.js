4755bfe7f86f89125d58bc28e5256d461ee1197e
Moshe Melamed <melamed2@gmail.com>
2020-05-05 23:19:01 +0300

    Updates

---
+++ precache-manifest.f3bd4ef4c1a4e3fac8883d5c2d595d77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895c88aea0192aa1aa5cac32bad3162b",
+    "url": "/mosh-portfolio/index.html"
+  },
+  {
+    "revision": "83fd98dcde0c84d75873",
+    "url": "/mosh-portfolio/static/css/main.55025dcb.chunk.css"
+  },
+  {
+    "revision": "ace4457ee27fb8769a0c",
+    "url": "/mosh-portfolio/static/js/2.b5c12984.chunk.js"
+  },
+  {
+    "revision": "83fd98dcde0c84d75873",
+    "url": "/mosh-portfolio/static/js/main.73064935.chunk.js"
+  },
+  {
+    "revision": "80b5b99e34a5055ff65c",
+    "url": "/mosh-portfolio/static/js/runtime~main.a7cc8764.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/mosh-portfolio/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/mosh-portfolio/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "cbd4e701269338259ee0b39a0b768167",
+    "url": "/mosh-portfolio/static/media/RobotoCondensed-Bold.cbd4e701.ttf"
+  },
+  {
+    "revision": "d6fc5ec5403ec65bdaecfe44b7b84402",
+    "url": "/mosh-portfolio/static/media/RobotoCondensed-Italic.d6fc5ec5.ttf"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/mosh-portfolio/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mosh-portfolio/precache-manifest.f3bd4ef4c1a4e3fac8883d5c2d595d77.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mosh-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
